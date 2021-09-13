37f4ee2ede06fd4d6659d55c1989d5f0a7e7fc7e
VictorTPhan <71913548+VictorTPhan@users.noreply.github.com>
2021-09-12 22:37:03 -0700

    Added a new screen

---
+++ lib/ListViewDemoPage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ListViewDemoPage extends StatefulWidget {
+  const ListViewDemoPage({Key? key}) : super(key: key);
+
+  @override
+  _ListViewDemoPageState createState() => _ListViewDemoPageState();
+}
+
+class _ListViewDemoPageState extends State<ListViewDemoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
